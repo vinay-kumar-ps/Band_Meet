44f735c1f0a0aeeeec68a9334ee5c20916f45423
Anandhu4456 <anandhurajan56@gmail.com>
2024-04-04 16:33:08 +0530

    added clientresponse in utils/response

--- pkg/utils/response/response.go
+++ pkg/utils/response/response.go
@@ -1,1 +1,17 @@
 package response
+
+type Response struct {
+	StatusCode int         `json:"statuscode"`
+	Message    string      `json:"message"`
+	Data       interface{} `json:"data"`
+	Error      interface{} `json:"error"`
+}
+
+func ClientResponse(sts int, msg string, data interface{}, err interface{}) Response {
+	return Response{
+		StatusCode: sts,
+		Message:    msg,
+		Data:       data,
+		Error:      err,
+	}
+}
