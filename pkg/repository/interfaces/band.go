c8a8de55b5f56c24af8f61d30c87b5a5bf00c366
Anandhu4456 <anandhurajan56@gmail.com>
2024-04-04 17:01:46 +0530

    added bandsignup function and login in repo(band)

--- pkg/repository/interfaces/band.go
+++ pkg/repository/interfaces/band.go
@@ -1,1 +1,8 @@
 package interfaces
+
+import "github.com/Anandhu4456/band-meet/pkg/utils/models"
+
+type BandRepo interface{
+	BandSignup(bandSignup models.BandSignup)(models.BandLoginResponse,error)
+	BandLogin(bandLogin models.BandLogin)(models.BandLoginResponse,error)
+}
