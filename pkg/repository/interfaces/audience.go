a99b92a7499027cd46c2dbc3101b493f91d23387
vinay-kumar-ps <vinaykumarps321@gmail.com>
2024-04-05 21:30:36 +0530

    added audience features and fixed typo in  in models folder

--- pkg/repository/interfaces/audience.go
+++ pkg/repository/interfaces/audience.go
@@ -1,5 +1,11 @@
 package interfaces
 
-type AudienceRepo interface{
-	
+import (
+
+
+	"github.com/Anandhu4456/band-meet/pkg/utils/models"
+)
+
+type UserRepo interface{
+	UserSignup( userSignup models.UserSignup) (models.UserLoginResponse,error)
 }
