8ec22a7ae0f85a49c89a555290b86606a11d47d1
Anandhu4456 <anandhurajan56@gmail.com>
2024-04-06 12:01:46 +0530

    merged user signup function in repo

--- pkg/repository/audience.go
+++ pkg/repository/audience.go
@@ -7,18 +7,19 @@ import (
 )
 
 type UserRepo struct {
-DB *gorm.DB
+	DB *gorm.DB
 }
+
 func NewUserRepo(db *gorm.DB) interfaces.UserRepo {
 	return &UserRepo{
 		DB: db,
 	}
 }
-func (u *UserRepo) UserSignup( userSignup models.UserSignup) (models.UserLoginResponse,error){
-	var  userLoginResponse models.UserLoginResponse
-	err := u.DB.Exec("INSERT INTO users(username,name,email,phone,password)Values(?,?,?,?,?)RETURNING(username,id,email,phone)",userSignup.UserName,userSignup.Name,userSignup.Email,userSignup.Phone,userSignup.Password).Scan(&userLoginResponse).Error
-	if err !=nil{
-		return models.UserLoginResponse{},err
+func (u *UserRepo) UserSignup(userSignup models.UserSignup) (models.UserLoginResponse, error) {
+	var userLoginResponse models.UserLoginResponse
+	err := u.DB.Exec("INSERT INTO users(username,name,email,phone,password)Values(?,?,?,?,?)RETURNING(username,id,email,phone)", userSignup.UserName, userSignup.Name, userSignup.Email, userSignup.Phone, userSignup.Password).Scan(&userLoginResponse).Error
+	if err != nil {
+		return models.UserLoginResponse{}, err
 	}
-	return userLoginResponse,nil
+	return userLoginResponse, nil
 }
--- pkg/repository/interfaces/band.go
+++ pkg/repository/interfaces/band.go
@@ -4,4 +4,5 @@ import "github.com/Anandhu4456/band-meet/pkg/utils/models"
 
 type BandRepo interface{
 	BandSignup(bandSignup models.BandSignup)(models.BandLoginResponse,error)
+	
 }
