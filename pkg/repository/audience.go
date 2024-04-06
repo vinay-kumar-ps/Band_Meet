7e6afd7e0680eb616ffcd4bd8ec24d5393073ed0
Anandhu4456 <anandhurajan56@gmail.com>
2024-04-06 12:18:07 +0530

    capitalised

--- pkg/repository/audience.go
+++ pkg/repository/audience.go
@@ -17,7 +17,7 @@ func NewUserRepo(db *gorm.DB) interfaces.UserRepo {
 }
 func (u *UserRepo) UserSignup(userSignup models.UserSignup) (models.UserLoginResponse, error) {
 	var userLoginResponse models.UserLoginResponse
-	err := u.DB.Exec("INSERT INTO users(username,name,email,phone,password)Values(?,?,?,?,?)RETURNING(username,id,email,phone)", userSignup.UserName, userSignup.Name, userSignup.Email, userSignup.Phone, userSignup.Password).Scan(&userLoginResponse).Error
+	err := u.DB.Exec("INSERT INTO users(username,name,email,phone,password)VALUES(?,?,?,?,?)RETURNING(username,id,email,phone)", userSignup.UserName, userSignup.Name, userSignup.Email, userSignup.Phone, userSignup.Password).Scan(&userLoginResponse).Error
 	if err != nil {
 		return models.UserLoginResponse{}, err
 	}
